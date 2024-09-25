ec9e0037fe3ec45f82f6cf967acf504d1b59ae21
AnilMetakari <anilmetakariamm@gmail.com>
2024-09-25 23:08:32 +0530

    Polymorphism as 25/09/2024

---
+++ Arrayassinment/DynamicPolym0rphism.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayassinment
+{
+    class Animal
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a shape ");
+
+        }
+    }
+    class Circle : Animal
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Circle");
+        }
+
+    }
+    class Rectange:Animal
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Rectange");
+        }
+    }
+    internal class DynamicPolym0rphism
+    {
+        static void Main()
+        {
+            Animal obj1 = new Animal();
+            Animal obj2 = new Circle();
+            Animal obj3 = new Rectange();
+            obj1.Draw();
+            obj2.Draw();
+            obj3.Draw();
+        }
+    }
+}
---
+++ Arrayassinment/Static_Polymorphism.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayassinment
+{
+    class Calculator
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+        public int add(int x,int y, int z)
+        {
+            return x + y + z;   
+        }
+        public double add(double x, double y, double z) 
+        {
+            return x + y + z; 
+        }
+
+    }
+    internal class Static_Polymorphism
+    {
+        static void Main()
+        {
+            Calculator c = new Calculator();
+            int sum1 = c.add(10, 20);
+            int sum2 = c.add(10, 20, 30);
+            double sum3 = c.add(10.5, 20.5, 30.5);
+            Console.WriteLine("Sum of two int number:"+sum1);
+            Console.WriteLine("Sum of three int number:"+sum2);
+            Console.WriteLine("Sum of three double number:"+sum3);
+        }
+
+    }
+}
