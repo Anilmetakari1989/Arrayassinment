bdaf91d4535cb1d386c25e1c031180a8dc0c6d1c
AnilMetakari <anilmetakariamm@gmail.com>
2024-10-15 21:54:58 +0530

    Assignment of Stack and Sictionary as 03/10/2024

---
+++ Arrayassinment/DictionaryExmple.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayassinment
+{
+    internal class DictionaryExmple
+    {
+        static void Main(String[] args)
+        {
+            Dictionary<string,int> dict= new Dictionary<string,int>();
+            dict.Add("Sachin", 21);
+            dict.Add("Rahul", 22);
+            dict.Add("Virat",23);
+            dict.Add("Rohit", 24);
+            dict.Add("Dipak", 25);
+            dict.Add("Anil", 27);
+            dict.Add("Sanjay", 26);
+            dict.Add("Kapil", 29);
+            dict.Add("laxman", 28);
+            Console.WriteLine("Total Elements from Dictionary ");
+            foreach(KeyValuePair<string,int> kvp in dict)
+            {
+                Console.WriteLine($"key is :{kvp.Key},value is:{kvp.Value} ");
+
+            }
+            dict["Sachin"] = 31;
+            dict["Anil"] = 32;
+            Console.WriteLine();
+            Console.WriteLine(" After Modified Elements from Disctionary");
+            foreach (KeyValuePair<string, int> kvp in dict)
+            {
+                Console.WriteLine($"key is :{kvp.Key},value is:{kvp.Value} ");
+
+            }Console.WriteLine();
+            Console.WriteLine("Removed the specfic Elements from Dictionary");
+            dict.Remove("Virat");
+            dict.Remove("Dipak");
+
+            dict.Remove("Laxman");
+            foreach (KeyValuePair<string, int> kvp in dict)
+            {
+                Console.WriteLine($"key is :{kvp.Key},value is:{kvp.Value} ");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Filter the elements from Dictionary");
+            if (dict.ContainsKey("Sachin"))
+            {
+                Console.WriteLine("Name is exit in Dictionary");
+            }
+                dict.Clear();
+
+            Console.WriteLine("Total count of elements :" + dict.Count);
+
+
+
+        }
+    }
+}
---
+++ Arrayassinment/StackExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayassinment
+{
+    internal class StackExample
+    {
+        static void Main()
+        {
+            Stack stack= new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push("HELLO");
+            stack.Push(9975357411);
+            stack.Push(false);
+            stack.Push(10.5);
+            Console.WriteLine("Total Elements in Stack ");
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Removed Elements from Stack");
+            object firstElement = stack.Pop(); 
+            object secondElement = stack.Pop();
+            object thirdElement = stack.Pop();
+            object forthElement = stack.Pop();
+            object fifthElement = stack.Pop();
+            object sixthElement = stack.Pop();
+            Console.WriteLine(firstElement);
+            Console.WriteLine(secondElement);
+            Console.WriteLine(thirdElement);
+            Console.WriteLine(forthElement);
+            Console.WriteLine(fifthElement);
+            Console.WriteLine(sixthElement);
+            Console.WriteLine("Is stack empty:"+(stack.Count==0));
+
+        }
+    }
+}
